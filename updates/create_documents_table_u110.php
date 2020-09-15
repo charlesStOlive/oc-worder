9d0c854d953bbf3c99457a152d5dbd1b6a72ea71
Charles <charles.stolive@gmail.com>
2020-09-15 16:02:13 +0200

    modifs scopes

--- updates/create_documents_table_u110.php
+++ updates/create_documents_table_u110.php
@@ -9,14 +9,14 @@ class CreateDocumentsTableU110 extends Migration
     public function up()
     {
         Schema::table('waka_worder_documents', function (Blueprint $table) {
-            $table->text('scope_type')->nullable();
+            $table->text('is_scope')->nullable();
         });
     }
 
     public function down()
     {
         Schema::table('waka_worder_documents', function (Blueprint $table) {
-            $table->dropColumn('scope_type');
+            $table->dropColumn('is_scope');
         });
     }
 }
