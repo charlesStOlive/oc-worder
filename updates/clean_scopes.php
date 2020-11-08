192cc69b4dbbfa9a371a33f2e33dd38d475928d7
Charles <charles.stolive@gmail.com>
2020-11-08 14:53:10 +0100

    wconfig

--- updates/clean_scopes.php
+++ updates/clean_scopes.php
@@ -4,16 +4,10 @@
 use Seeder;
 use Waka\Worder\Models\Document;
 
-//use System\Models\File;
-//use Waka\Worder\Models\BlocType;
-
-// use Waka\Crsm\Classes\CountryImport;
-
 class CleanScopes extends Seeder
 {
     public function run()
     {
-        //$this->call('Waka\Crsm\Updates\Seeders\SeedWorder');
         Document::where('scopes', '<>', null)->update(['scopes' => null]);
 
     }
