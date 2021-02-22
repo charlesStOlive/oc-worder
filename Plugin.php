942343584a677f034be5b634c20a3278c866fe13
Charles <charles.stolive@gmail.com>
2021-02-22 10:38:24 +0100

    PhpWord dans le plugin & Wcli update

--- Plugin.php
+++ Plugin.php
@@ -88,6 +88,40 @@ class Plugin extends PluginBase
 
     }
 
+    public function bootPackages()
+    {
+        // Get the namespace of the current plugin to use in accessing the Config of the plugin
+        $pluginNamespace = str_replace('\\', '.', strtolower(__NAMESPACE__));
+
+        // Instantiate the AliasLoader for any aliases that will be loaded
+        $aliasLoader = AliasLoader::getInstance();
+
+        // Get the packages to boot
+        $packages = Config::get($pluginNamespace . '::packages');
+
+        // Boot each package
+        foreach ($packages as $name => $options) {
+            // Setup the configuration for the package, pulling from this plugin's config
+            if (!empty($options['config']) && !empty($options['config_namespace'])) {
+                Config::set($options['config_namespace'], $options['config']);
+            }
+
+            // Register any Service Providers for the package
+            if (!empty($options['providers'])) {
+                foreach ($options['providers'] as $provider) {
+                    App::register($provider);
+                }
+            }
+
+            // Register any Aliases for the package
+            if (!empty($options['aliases'])) {
+                foreach ($options['aliases'] as $alias => $path) {
+                    $aliasLoader->alias($alias, $path);
+                }
+            }
+        }
+    }
+
     /**
      * Registers any front-end components implemented in this plugin.
      *
