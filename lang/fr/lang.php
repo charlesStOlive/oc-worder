567c9b4270ac440a40a27fb6c70e0b60a2ea7bac
Charles <charles@notilac.fr>
2023-04-07 16:16:47 +0200

    MAJ LANG

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -1,32 +1,33 @@
 <?php
 
 return [
-    "document" => [
-        "scopes" => [
-            "id" => "ID recherché",
-            "id_com" => "Vous pouvez ajouter plusieurs ID",
-            "self" => "Fonction de restriction liée à l'id de ce modèle ?",
-            "target" => "Relation de la cible",
-            "target_com" => "Ecrire le nom de la relation les relations parentes ne sont pas disponible"
-        ]
+    'document' => [
+        'scopes' => [
+            'id' => 'ID recherché',
+            'id_com' => 'Vous pouvez ajouter plusieurs ID',
+            'self' => 'Fonction de restriction liée à l\'id de ce modèle ?',
+            'target' => 'Relation de la cible',
+            'target_com' => 'Ecrire le nom de la relation les relations parentes ne sont pas disponible'
+        ],
+        'title' => 'Créer document'
     ],
-    "menu" => [
-        "bloc_type" => "Type de bloc",
-        "bloc_type_description" => "Description",
-        "documents" => "Words",
-        "documents_description" => "Gestion des modèles words",
-        "settings_category" => "Wakaari Modèle"
+    'menu' => [
+        'bloc_type' => 'Type de bloc',
+        'bloc_type_description' => 'Description',
+        'documents' => 'Words',
+        'documents_description' => 'Gestion des modèles words',
+        'settings_category' => 'Wakaari Modèle'
     ],
-    "word" => [
-        "error" => null,
-        "processor" => [
-            "bad_format" => "Fromat du tag incorrect",
-            "bad_tag" => "Le type de tag est incorrect",
-            "document_not_exist" => " La source du document n'a pas été trouvé",
-            "errors" => "Ce document à des erreurs, veuillez les corriger.",
-            "field_not_existe" => "Le champs n'existe pas",
-            "id_not_exist" => "L'id n'existe pas",
-            "success" => "Le systhème n'a pas trouvé d'erreurs. Pensez à verifier votre document après édition"
+    'word' => [
+        'error' => null,
+        'processor' => [
+            'bad_format' => 'Fromat du tag incorrect',
+            'bad_tag' => 'Le type de tag est incorrect',
+            'document_not_exist' => ' La source du document n\'a pas été trouvé',
+            'errors' => 'Ce document à des erreurs, veuillez les corriger.',
+            'field_not_existe' => 'Le champs n\'existe pas',
+            'id_not_exist' => 'L\'id n\'existe pas',
+            'success' => 'Le systhème n\'a pas trouvé d\'erreurs. Pensez à verifier votre document après édition'
         ]
     ]
 ];
